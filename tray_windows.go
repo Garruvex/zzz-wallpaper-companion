fa85b1af58e5a5489136ac9d3911ddace0ee012c
Red <redxwolfski@gmail.com>
2026-08-06 17:20:56 +0800

    Add core Go application with config store, HTTP server, and tray support

---
+++ tray_windows.go
@@ -0,0 +1,227 @@
+//go:build windows
+
+package main
+
+import (
+	"context"
+	_ "embed"
+	"os/exec"
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+//go:embed winres/icon.ico
+var trayIconICO []byte
+
+const (
+	wmDestroy       = 0x0002
+	wmCommand       = 0x0111
+	wmApp           = 0x8000
+	wmTray          = wmApp + 1
+	wmRButtonUp     = 0x0205
+	wmLButtonDblClk = 0x0203
+
+	nimAdd     = 0x00000000
+	nimDelete  = 0x00000002
+	nifMessage = 0x00000001
+	nifIcon    = 0x00000002
+	nifTip     = 0x00000004
+
+	idiApplication = 32512
+	imageIcon      = 1
+	lrDefaultSize  = 0x0040
+	mfString       = 0x00000000
+	tpmRightButton = 0x0002
+	cwUseDefault   = 0x80000000
+	cmdSettings    = 1001
+	cmdUpdate      = 1002
+	cmdLogs        = 1003
+	cmdQuit        = 1004
+)
+
+type point struct{ x, y int32 }
+type msg struct {
+	hwnd           uintptr
+	message        uint32
+	wParam, lParam uintptr
+	time           uint32
+	pt             point
+}
+type wndClassEx struct {
+	size                               uint32
+	style                              uint32
+	wndProc                            uintptr
+	clsExtra, wndExtra                 int32
+	instance, icon, cursor, background uintptr
+	menuName, className                *uint16
+	iconSmall                          uintptr
+}
+type notifyIconData struct {
+	size             uint32
+	hwnd             uintptr
+	id               uint32
+	flags            uint32
+	callbackMessage  uint32
+	icon             uintptr
+	tip              [128]uint16
+	state, stateMask uint32
+	info             [256]uint16
+	timeoutOrVersion uint32
+	infoTitle        [64]uint16
+	infoFlags        uint32
+	guid             [16]byte
+	balloonIcon      uintptr
+}
+
+var (
+	user32              = syscall.NewLazyDLL("user32.dll")
+	shell32             = syscall.NewLazyDLL("shell32.dll")
+	kernel32            = syscall.NewLazyDLL("kernel32.dll")
+	procDefWindowProc   = user32.NewProc("DefWindowProcW")
+	procPostQuitMessage = user32.NewProc("PostQuitMessage")
+	trayPort            int
+	trayDataDir         string
+	trayResolver        *Resolver
+	trayQuit            func()
+)
+
+func runTray(port int, dataDir string, resolver *Resolver, quit func()) error {
+	trayPort, trayDataDir, trayResolver, trayQuit = port, dataDir, resolver, quit
+	instance, _, _ := kernel32.NewProc("GetModuleHandleW").Call(0)
+	className, _ := syscall.UTF16PtrFromString("ZZZWallpaperCompanionTray")
+	wndProc := syscall.NewCallback(windowProc)
+	wc := wndClassEx{size: uint32(unsafe.Sizeof(wndClassEx{})), wndProc: wndProc, instance: instance, className: className}
+	atom, _, registerErr := user32.NewProc("RegisterClassExW").Call(uintptr(unsafe.Pointer(&wc)))
+	if atom == 0 {
+		return registerErr
+	}
+	hwnd, _, createErr := user32.NewProc("CreateWindowExW").Call(0, uintptr(unsafe.Pointer(className)), uintptr(unsafe.Pointer(className)), 0, cwUseDefault, cwUseDefault, 0, 0, 0, 0, instance, 0)
+	if hwnd == 0 {
+		return createErr
+	}
+	icon := loadTrayIcon(instance)
+	data := notifyIconData{size: uint32(unsafe.Sizeof(notifyIconData{})), hwnd: hwnd, id: 1, flags: nifMessage | nifIcon | nifTip, callbackMessage: wmTray, icon: icon}
+	copy(data.tip[:], syscall.StringToUTF16("ZZZ Wallpaper Companion"))
+	ok, _, addErr := shell32.NewProc("Shell_NotifyIconW").Call(nimAdd, uintptr(unsafe.Pointer(&data)))
+	if ok == 0 {
+		return addErr
+	}
+	defer shell32.NewProc("Shell_NotifyIconW").Call(nimDelete, uintptr(unsafe.Pointer(&data)))
+	var message msg
+	for {
+		result, _, err := user32.NewProc("GetMessageW").Call(uintptr(unsafe.Pointer(&message)), 0, 0, 0)
+		if int32(result) == -1 {
+			return err
+		}
+		if result == 0 {
+			return nil
+		}
+		user32.NewProc("TranslateMessage").Call(uintptr(unsafe.Pointer(&message)))
+		user32.NewProc("DispatchMessageW").Call(uintptr(unsafe.Pointer(&message)))
+	}
+}
+
+func windowProc(hwnd uintptr, message uint32, wParam, lParam uintptr) uintptr {
+	switch message {
+	case wmTray:
+		if lParam == wmRButtonUp {
+			showTrayMenu(hwnd)
+		}
+		if lParam == wmLButtonDblClk {
+			openURL(settingsURL(trayPort))
+		}
+		return 0
+	case wmCommand:
+		switch uint16(wParam) {
+		case cmdSettings:
+			openURL(settingsURL(trayPort))
+		case cmdLogs:
+			_ = exec.Command("explorer.exe", trayDataDir).Start()
+		case cmdUpdate:
+			go func() {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+				defer cancel()
+				_ = trayResolver.Update(ctx)
+			}()
+		case cmdQuit:
+			trayQuit()
+			user32.NewProc("DestroyWindow").Call(hwnd)
+		}
+		return 0
+	case wmDestroy:
+		procPostQuitMessage.Call(0)
+		return 0
+	}
+	result, _, _ := procDefWindowProc.Call(hwnd, uintptr(message), wParam, lParam)
+	return result
+}
+
+func showTrayMenu(hwnd uintptr) {
+	menu, _, _ := user32.NewProc("CreatePopupMenu").Call()
+	if menu == 0 {
+		return
+	}
+	defer user32.NewProc("DestroyMenu").Call(menu)
+	appendMenu(menu, cmdSettings, "Settings")
+	appendMenu(menu, cmdUpdate, "Check for yt-dlp updates")
+	appendMenu(menu, cmdLogs, "Open data folder")
+	appendMenu(menu, cmdQuit, "Quit")
+	var cursor point
+	user32.NewProc("GetCursorPos").Call(uintptr(unsafe.Pointer(&cursor)))
+	user32.NewProc("SetForegroundWindow").Call(hwnd)
+	user32.NewProc("TrackPopupMenu").Call(menu, tpmRightButton, uintptr(cursor.x), uintptr(cursor.y), 0, hwnd, 0)
+}
+
+func appendMenu(menu uintptr, id uint16, label string) {
+	text, _ := syscall.UTF16PtrFromString(label)
+	user32.NewProc("AppendMenuW").Call(menu, mfString, uintptr(id), uintptr(unsafe.Pointer(text)))
+}
+
+func loadTrayIcon(instance uintptr) uintptr {
+	icon, _, _ := user32.NewProc("LoadImageW").Call(instance, 1, imageIcon, 0, 0, lrDefaultSize)
+	if icon != 0 {
+		return icon
+	}
+	if icon := iconFromICO(trayIconICO); icon != 0 {
+		return icon
+	}
+	icon, _, _ = user32.NewProc("LoadIconW").Call(0, idiApplication)
+	return icon
+}
+
+func iconFromICO(data []byte) uintptr {
+	if len(data) < 22 {
+		return 0
+	}
+	lookup := user32.NewProc("LookupIconIdFromDirectoryEx")
+	create := user32.NewProc("CreateIconFromResourceEx")
+	id, _, _ := lookup.Call(uintptr(unsafe.Pointer(&data[0])), 1, 32, 32, lrDefaultSize)
+	if id == 0 {
+		return 0
+	}
+	if int(id) >= len(data) {
+		return 0
+	}
+	iconBits := data[id:]
+	icon, _, _ := create.Call(
+		uintptr(unsafe.Pointer(&iconBits[0])),
+		uintptr(len(iconBits)),
+		1,
+		0x00030000,
+		32,
+		32,
+		0,
+	)
+	return icon
+}
+
+func openURL(url string) {
+	_ = exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", url).Start()
+}
+
+func fatalDialog(title, message string) {
+	t, _ := syscall.UTF16PtrFromString(title)
+	m, _ := syscall.UTF16PtrFromString(message)
+	user32.NewProc("MessageBoxW").Call(0, uintptr(unsafe.Pointer(m)), uintptr(unsafe.Pointer(t)), 0x10)
+}
